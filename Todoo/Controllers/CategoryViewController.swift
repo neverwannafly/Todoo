fdc40bc03df68f44fbf9354b630f162180e2633f
Shubham Anand <shubham_chess@live.com>
2018-07-18 02:28:28 +0530

    Add new relational model.

---
+++ Todoo/Controllers/CategoryViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CategoryViewController.swift
+//  Todoo
+//
+//  Created by Shubham Anand on 18/07/18.
+//  Copyright © 2018 Shubham Anand. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        
+        
+    }
+    
+    //MARK: - TableView Datasource method
+    
+    //MARK: - TableView Delegate method
+    
+    //MARK: - Querying the Data
+
+}
