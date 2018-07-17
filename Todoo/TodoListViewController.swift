dd9948c157a0d88e872c5d933c4ad9d7559a77bb
Shubham Anand <shubham_chess@live.com>
2018-07-17 22:15:23 +0530

    Add function to add more tasks to todo list

--- Todoo/TodoListViewController.swift
+++ Todoo/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController : UITableViewController {
 
-    let itemArray = ["Find Mike", "Destroy Asgard", "Buy eggs"]
+    var itemArray = ["Find Mike", "Destroy Asgard", "Buy eggs"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,6 +41,32 @@ class TodoListViewController : UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    //MARK - Add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Task", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Task", style: .default) { (action) in
+            //What will happen once the user clicks the Add Item Button on our UIAlert
+            if textField.text != nil && textField.text != "" {
+                self.itemArray.append(textField.text!)
+                print(textField.text!)
+                self.tableView.reloadData()
+            }
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Task"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 
 }
 
